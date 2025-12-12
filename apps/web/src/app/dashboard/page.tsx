94f77a7d5d4adc8114fc023e39a7e17ddfb41701
otais <alvesbernardino@gmail.com>
2025-12-12 12:46:02 +0000

    testa quando crio papel

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,9 +1,6 @@
-import { authorizePapelOrRedirect } from "@repo/authweb/autorizado";
 import React from "react";
 
 const DashBoard = async () => {
-  await authorizePapelOrRedirect("");
-
   return <div>Dashboard</div>;
 };
 
